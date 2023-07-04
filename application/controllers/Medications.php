d079cfee80000e026b3997feddab3020e43edd53
MonikaWaweru <121816772+MonikaWaweru@users.noreply.github.com>
2023-07-04 13:44:36 +0300

    Update and rename Expenses.php to Medications.php

--- application/controllers/Medications.php
+++ application/controllers/Medications.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Expenses extends CI_Controller {
+class Medications extends CI_Controller {
 //Validating login
 function __construct(){
 parent::__construct();
