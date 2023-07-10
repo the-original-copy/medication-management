72acb499713b49f5db47ccec5211158493fe31e7
MonikaWaweru <121816772+MonikaWaweru@users.noreply.github.com>
2023-07-10 22:18:32 +0300

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -7,7 +7,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span></button>
-                <a class="navbar-brand" href="dashboard.php"><span>MEDIAPP</span></a>
+                <a class="navbar-brand" href="dashboard.php"><span>MediApp</span></a>
                 
             </div>
             
