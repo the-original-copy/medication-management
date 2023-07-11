6c77b7bc116e952c32da0747c88824b92346067c
MonikaWaweru <121816772+MonikaWaweru@users.noreply.github.com>
2023-07-11 21:03:42 +0300

    Update add-medication.php

--- application/views/add-medication.php
+++ application/views/add-medication.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Add Medication</title>
+	<title>MediApp||Add Medication</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 
