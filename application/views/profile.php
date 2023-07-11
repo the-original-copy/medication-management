de6ec654b25a5ee87bd26caf0beeb7a5561eea2b
MonikaWaweru <121816772+MonikaWaweru@users.noreply.github.com>
2023-07-11 21:05:46 +0300

    Update profile.php

--- application/views/profile.php
+++ application/views/profile.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>User Profile</title>
+	<title>MediApp||User Profile</title>
 <?php echo link_tag('assets/css/bootstrap.min.css')?>
 	<?php echo link_tag('assets/css/datepicker3.css')?>
 	<?php echo link_tag('assets/css/styles.css')?>
